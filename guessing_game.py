ec59e8355e51c80f2b33d7cc055f347bdf18a5d3
Rushi0411 <41-bit-0022@kdu.ac.lk>
2025-05-03 17:37:10 +0530

    Fixed conflict: using range 1-75

--- guessing_game.py
+++ guessing_game.py
@@ -1,6 +1,6 @@
 import random 
-number = random.randint(1, 100) 
-print("Guess a number between 1 and 100") 
+number = random.randint(1, 75) 
+print("Guess a number between 1 and 75") 
 guess = int(input()) 
 if guess == number: 
  print("You win!") 
