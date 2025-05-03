a17d39a6a8a89c3cdd161712f4e03306912f1b37
Rushi0411 <41-bit-0022@kdu.ac.lk>
2025-05-03 17:12:14 +0530

    Add while True: loop

---
+++ guessing_game.py
@@ -0,0 +1,14 @@
+import random 
+number = random.randint(1, 10) 
+print("Guess a number between 1 and 10") 
+guess = int(input()) 
+if guess == number: 
+ print("You win!") 
+else: 
+ print(f"Wrong! The number was {number}")
+
+ while True: 
+    # (paste existing code here) 
+    print("Play again? (y/n)") 
+    if input().lower() != 'y': 
+        break
