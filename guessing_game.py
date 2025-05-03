c84472c9ac409e7aa5f522fe8cdd25d37b7ab8e9
sayuni02 <41-bit-0052@kdu.ac.lk>
2025-05-03 17:37:07 +0530

    Added [your feature]

--- guessing_game.py
+++ guessing_game.py
@@ -1,18 +1,8 @@
 import random 
-<<<<<<< HEAD
-number = random.randint(1, 50)
-print("Guess a number between 1 and 50")
-=======
-number = random.randint(1, 100) 
-print("Guess a number between 1 and 100") 
->>>>>>> 05ec786a9b659bb1005c94a9b6105ecdc340a952
+number = random.randint(1, 75) 
+print("Guess a number between 1 and 75") 
 guess = int(input()) 
 if guess == number: 
- print("You win!") 
+    print("You win!") 
 else: 
-<<<<<<< HEAD
-    print(f"Wrong! The number was {number}")
-=======
- print(f"Wrong! The number was {number}") 
-
->>>>>>> 05ec786a9b659bb1005c94a9b6105ecdc340a952
+    print(f"Wrong! The number was {number}") 
